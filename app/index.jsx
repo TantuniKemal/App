faa002068dba716c588d16ab9e8dade53fb76601
Furkan Deniz <mr0wido@protonmail.com>
2024-08-11 17:42:48 +0300

    New

--- app/index.jsx
+++ app/index.jsx
@@ -7,6 +7,7 @@ export default function App(){
     return (
         <View className="flex-1 items-center justify-center bg-white">
             <Text className="text-3xl font-bold">1asdsa1233</Text>
+            <Text className="text-3xl font-bold">1asdsa1233</Text>
             <StatusBar style="auto"/>
             <Link href="/profile" style={{color: 'blue'}} >Go to Profile</Link>
         </View>
