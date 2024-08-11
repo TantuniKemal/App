80d8ddd62a1182bebfb2dac932ecbee9007137b3
“[TantuniKemal]” <[oztk.erkn@gmail.com]>
2024-08-11 21:46:38 +0300

    change1

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,12 +1,59 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from 'react-native'
+import { Tabs, Redirect } from 'expo-router'
+import { icons } from '../../constants';
 
-const RooyLayout = () => {
+const TabIcon = ({icon, color, name, focused}) => {
+    return(
+        <View className="items-center justify-center gap-0">
+            <Image
+            source={icon}
+            resizeMode='contain'
+            tintColor={color}
+            className="w-6 h-6"
+            />
+            <Text className={`${focused ? 'font-psemibold':
+                'font-regular'} text-xs`} style={{color:color}}>
+            
+                 {name}
+            </Text>
+        </View>
+    )
+}
+
+const TabsLayout = () => {
   return (
-    <View>
-      <Text>RooyLayout</Text>
-    </View>
+   <>
+    <Tabs screenOptions={{
+        tabBarShowLabel: false,
+        tabBarActiveTintColor:'#FFA001',
+        tabBarInactiveTintColor: '#CDCDE0',
+        tabBarStyle:{
+          backgroundColor: '#F5EFF7',
+          borderTopWidth: 1,
+          borderTopColor: '#232533',
+          height: 84,
+        }
+       }}
+       >    
+        <Tabs.Screen
+            name="home"
+            options={{
+                title:"Home",
+                headerShown: false,
+            tabBarIcon:({icon, focused}) => {
+                <TabIcon
+                    
+                />
+
+
+
+            }
+            }}
+        />
+
+    </Tabs>
+   </>
   )
 }
 
-export default RooyLayout
+export default TabsLayout
