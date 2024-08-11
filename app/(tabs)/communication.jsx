71716986956637b0ae5c86eecab33a76e70e94ff
“[TantuniKemal]” <[oztk.erkn@gmail.com]>
2024-08-11 20:20:07 +0300

    new101

---
+++ app/(tabs)/communication.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Communication = () => {
+  return (
+    <View>
+      <Text>İletişim</Text>
+    </View>
+  )
+}
+
+export default Communication
---
+++ app/(tabs)/profile.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <View>
+      <Text>Hesabım</Text>
+    </View>
+  )
+}
+
+export default Profile
