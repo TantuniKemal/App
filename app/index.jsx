d6ef6ff72ffe6b442ad7a18db62a71a453f74f59
“[TantuniKemal]” <[oztk.erkn@gmail.com]>
2024-08-11 17:24:05 +0300

    new

--- app/index.jsx
+++ app/index.jsx
@@ -6,7 +6,7 @@ import React from 'react';
 export default function App(){
     return (
         <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-3xl font-bold">Hello Worlddd</Text>
+            <Text className="text-3xl font-bold">Merhaba</Text>
             <StatusBar style="auto"/>
             <Link href="/profile" style={{color: 'blue'}} >Go to Profile</Link>
         </View>
