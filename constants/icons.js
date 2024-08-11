0db3cb696cb0b5a4a7226b942c8a588e162d5bd8
“[TantuniKemal]” <[oztk.erkn@gmail.com]>
2024-08-11 20:44:58 +0300

    newConstants

---
+++ constants/icons.js
@@ -0,0 +1,30 @@
+import change from '../assets/icons/change.png';
+import heart from '../assets/icons/heart.png';
+import home from '../assets/icons/home.png';
+import mail from '../assets/icons/mail.png';
+import map from '../assets/icons/map.png';
+import menu from '../assets/icons/menu.png';
+import message from '../assets/icons/message.png';
+import password from '../assets/icons/password.png';
+import phone from '../assets/icons/phone.png';
+import plus from '../assets/icons/plus.png';
+import shopping from '../assets/icons/shopping.png';
+import smartPhone from '../assets/icons/smartPhone.png';
+import user from '../assets/icons/user.png';
+
+export default{
+     change,
+     heart,
+     home,
+     mail,
+     map,
+     menu,
+     message,
+     password,
+     phone,
+     plus,
+     shopping,
+     smartPhone,
+     user,
+};
+
